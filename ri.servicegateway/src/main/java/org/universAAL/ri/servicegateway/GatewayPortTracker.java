ef528693fac1fdef673a9b96852fee3764a125fb
mr Andrej Grguric <andrej.grguric@ericsson.com>
2011-11-24 14:46:09 +0000

    updated to middleware version 0.3.1

--- ri.servicegateway/src/main/java/org/universAAL/ri/servicegateway/GatewayPortTracker.java
+++ ri.servicegateway/src/main/java/org/universAAL/ri/servicegateway/GatewayPortTracker.java
@@ -40,174 +40,176 @@ import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.universAAL.middleware.container.ModuleContext;
 
 public class GatewayPortTracker extends ServiceTracker implements Runnable {
 
-    private static int SLEEP_TIME = 3000;
-    private static final Logger log = LoggerFactory
-	    .getLogger(GatewayPortTracker.class);
-    private HttpService httpService;
-    private BundleContext bc;
-    private Thread thread;
-    private Vector<ServiceReference> servletToBeRegistered;
-    private ServiceTracker httpTracker;
-    private Set<String> urlSet, dataDirSet;
+	private static int SLEEP_TIME = 3000;
+	private static final Logger log = LoggerFactory
+			.getLogger(GatewayPortTracker.class);
+	private HttpService httpService;
+	private BundleContext bc;
+	private ModuleContext moduleContext;
+	private Thread thread;
+	private Vector<ServiceReference> servletToBeRegistered;
+	private ServiceTracker httpTracker;
+	private Set<String> urlSet, dataDirSet;
 
-    private class GatewayHttpContext implements HttpContext {
+	private class GatewayHttpContext implements HttpContext {
 
-	private ServiceReference reference;
+		private ServiceReference reference;
 
-	public GatewayHttpContext(ServiceReference reference) {
-	    this.reference = reference;
-	}
-
-	public String getMimeType(String name) {
-	    if (name.endsWith(".jpg") || name.endsWith(".jpeg")) {
-		return "image/jpeg";
-	    } else if (name.endsWith(".gif")) {
-		return "image/gif";
-	    } else if (name.endsWith(".html") || name.endsWith(".htm")) {
-		return "text/html";
-	    } else if (name.endsWith(".png")) {
-		return "image/png";
-	    } else if (name.endsWith(".css")) {
-		return "text/css";
-	    } else if (name.endsWith(".xml")) {
-		return "text/xml";
-	    } else if (name.endsWith(".js")) {
-		return "application/javascript";
-	    } else {
-		return null;
-	    }
-
-	}
-
-	public URL getResource(String res) {
-	    return reference.getBundle().getResource(res);
-	}
-
-	public boolean handleSecurity(HttpServletRequest arg0,
-		HttpServletResponse arg1) throws IOException {
-
-	    return true;
-	}
-
-    }
-
-    public GatewayPortTracker(BundleContext bc, String clazz,
-	    ServiceTrackerCustomizer stc) {
-	super(bc, clazz, stc);
-
-	servletToBeRegistered = new Vector<ServiceReference>();
-	urlSet = new HashSet<String>();
-	dataDirSet = new HashSet<String>();
-
-	this.bc = bc;
-	httpTracker = new ServiceTracker(bc, HttpService.class.getName(), null); // no
-	httpTracker.open();
-
-	thread = new Thread(this, "GatewayPortTracker");
-	thread.start();
-    }
-
-    public synchronized void registerService(ServiceReference reference)
-	    throws ServletException, NamespaceException {
-	GatewayPort gp = (GatewayPort) bc.getService(reference);
-	gp.setContext(reference.getBundle().getBundleContext());
-	HttpContext servletContext = new GatewayHttpContext(reference);
-	String url = gp.url();
-	String dataDir = gp.dataDir();
-
-	log.info("GATEWAY_TRACKER: Registering servlet, url -> " + url
-		+ " dataDir -> " + (dataDir == null ? "none" : dataDir));
-	// register the servlet to the url, for example /whereis, if it is
-	// already not registered
-	if (urlSet.add(url))
-	    httpService.registerServlet(url, gp, null, servletContext);
-
-	// register also the path for the resources, for example
-	// /helpwhenoutside, if not registered
-	if (dataDir != null && dataDirSet.add(dataDir))
-	    httpService.registerResources(dataDir, dataDir, servletContext);
-
-    }
-
-    public synchronized Object addingService(ServiceReference reference) {
-	// if the http service is still not available, store the services
-	// to be registered when it will be available
-	if (httpService == null) {
-	    servletToBeRegistered.add(reference);
-	    return null;
-	}
-
-	GatewayPort gp = null;
-	try {
-	    // if there is some previously stored service first register it
-	    if (!servletToBeRegistered.isEmpty()) {
-		registerPreviousServices();
-	    }
-
-	    registerService(reference);
-
-	} catch (NamespaceException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} catch (ServletException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-	return gp;
-    }
-
-    public synchronized void removedService(ServiceReference reference,
-	    Object service) {
-
-	System.out.println("Removed: " + reference.toString());
-	GatewayPort gp = (GatewayPort) service;
-	httpService.unregister(gp.url());
-
-	super.removedService(reference, service);
-
-    }
-
-    public void run() {
-	Thread currentThread = Thread.currentThread();
-	while (thread == currentThread) {
-	    // log.info("proba log");
-	    try {
-		HttpService httpService = (HttpService) httpTracker
-			.getService();
-		if (httpService != null) {
-		    this.httpService = httpService;
-		    registerPreviousServices();
-		    thread = null;
-		} else {
-		    Thread.sleep(SLEEP_TIME);
+		public GatewayHttpContext(ServiceReference reference) {
+			this.reference = reference;
 		}
-	    } catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    } catch (ServletException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    } catch (NamespaceException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
+
+		public String getMimeType(String name) {
+			if (name.endsWith(".jpg") || name.endsWith(".jpeg")) {
+				return "image/jpeg";
+			} else if (name.endsWith(".gif")) {
+				return "image/gif";
+			} else if (name.endsWith(".html") || name.endsWith(".htm")) {
+				return "text/html";
+			} else if (name.endsWith(".png")) {
+				return "image/png";
+			} else if (name.endsWith(".css")) {
+				return "text/css";
+			} else if (name.endsWith(".xml")) {
+				return "text/xml";
+			} else if (name.endsWith(".js")) {
+				return "application/javascript";
+			} else {
+				return null;
+			}
+
+		}
+
+		public URL getResource(String res) {
+			return reference.getBundle().getResource(res);
+		}
+
+		public boolean handleSecurity(HttpServletRequest arg0,
+				HttpServletResponse arg1) throws IOException {
+
+			return true;
+		}
+
 	}
 
-    }
+	public GatewayPortTracker(BundleContext bc, ModuleContext mcontext,
+			String clazz, ServiceTrackerCustomizer stc) {
+		super(bc, clazz, stc);
+		moduleContext = mcontext;
+		servletToBeRegistered = new Vector<ServiceReference>();
+		urlSet = new HashSet<String>();
+		dataDirSet = new HashSet<String>();
 
-    private synchronized void registerPreviousServices()
-	    throws ServletException, NamespaceException {
+		this.bc = bc;
+		httpTracker = new ServiceTracker(bc, HttpService.class.getName(), null); // no
+		httpTracker.open();
 
-	Iterator<ServiceReference> i = servletToBeRegistered.iterator();
-	while (i.hasNext()) {
-
-	    registerService(i.next());
+		thread = new Thread(this, "GatewayPortTracker");
+		thread.start();
 	}
-	servletToBeRegistered.clear();
 
-    }
+	public synchronized void registerService(ServiceReference reference)
+			throws ServletException, NamespaceException {
+		GatewayPort gp = (GatewayPort) bc.getService(reference);
+		gp.setContext(moduleContext);
+		HttpContext servletContext = new GatewayHttpContext(reference);
+		String url = gp.url();
+		String dataDir = gp.dataDir();
+
+		log.info("GATEWAY_TRACKER: Registering servlet, url -> " + url
+				+ " dataDir -> " + (dataDir == null ? "none" : dataDir));
+		// register the servlet to the url, for example /whereis, if it is
+		// already not registered
+		if (urlSet.add(url))
+			httpService.registerServlet(url, gp, null, servletContext);
+
+		// register also the path for the resources, for example
+		// /helpwhenoutside, if not registered
+		if (dataDir != null && dataDirSet.add(dataDir))
+			httpService.registerResources(dataDir, dataDir, servletContext);
+
+	}
+
+	public synchronized Object addingService(ServiceReference reference) {
+		// if the http service is still not available, store the services
+		// to be registered when it will be available
+		if (httpService == null) {
+			servletToBeRegistered.add(reference);
+			return null;
+		}
+
+		GatewayPort gp = null;
+		try {
+			// if there is some previously stored service first register it
+			if (!servletToBeRegistered.isEmpty()) {
+				registerPreviousServices();
+			}
+
+			registerService(reference);
+
+		} catch (NamespaceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return gp;
+	}
+
+	public synchronized void removedService(ServiceReference reference,
+			Object service) {
+
+		System.out.println("Removed: " + reference.toString());
+		GatewayPort gp = (GatewayPort) service;
+		httpService.unregister(gp.url());
+
+		super.removedService(reference, service);
+
+	}
+
+	public void run() {
+		Thread currentThread = Thread.currentThread();
+		while (thread == currentThread) {
+			// log.info("proba log");
+			try {
+				HttpService httpService = (HttpService) httpTracker
+						.getService();
+				if (httpService != null) {
+					this.httpService = httpService;
+					registerPreviousServices();
+					thread = null;
+				} else {
+					Thread.sleep(SLEEP_TIME);
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ServletException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NamespaceException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	private synchronized void registerPreviousServices()
+			throws ServletException, NamespaceException {
+
+		Iterator<ServiceReference> i = servletToBeRegistered.iterator();
+		while (i.hasNext()) {
+
+			registerService(i.next());
+		}
+		servletToBeRegistered.clear();
+
+	}
 
 }

8c87c971b677cb74b53e99dfc99cb23e01448929
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-06-21 16:06:11 +0000

    Automatic tag of ri.gateway.communicator version: 1.3.3-SNAPSHOT

---
+++ ri.gateway.communicator-1.3.3-SNAPSHOT/src/main/java/org/universAAL/ri/gateway/communicator/service/Message.java
@@ -0,0 +1,111 @@
+/*
+Copyright 2011-2014 AGH-UST, http://www.agh.edu.pl
+Faculty of Computer Science, Electronics and Telecommunications
+Department of Computer Science 
+
+See the NOTICE file distributed with this work for additional
+information regarding copyright ownership
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package org.universAAL.ri.gateway.communicator.service;
+
+import java.io.Serializable;
+
+/**
+ * This class represents a generic message sent between inside and outside world
+ * of AALSpace.
+ * 
+ * @author skallz
+ */
+public class Message implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -2259161945843015685L;
+
+    /**
+     * Content of the message.
+     */
+    private Object content;
+    
+    private String remoteProxyRegistrationId;
+    
+    private String remoteMemberId;
+    
+    /**
+     * Initializes with a message to send.
+     * 
+     * @param content
+     *            content of the message
+     */
+    public Message(final Object content) {
+	this.content = content;
+    }
+
+    /**
+     * Returns content of the message.
+     * 
+     * @return content of the message
+     */
+    public Object getContent() {
+	return content;
+    }
+
+    @Override
+    public String toString() {
+	if (content != null) {
+	    return "content: " + content.toString();
+	} else {
+	    return "content: " + "null";
+	}
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+	if (obj == null || !(obj instanceof Message)) {
+	    return false;
+	}
+	Message o = (Message) obj;
+	if (content == null && o.content == null) {
+	    return true;
+	}
+	return content.equals(o.content);
+    }
+
+    @Override
+    public int hashCode() {
+	if (content == null) {
+	    return 0;
+	}
+	return content.hashCode();
+    }
+
+    public String getRemoteProxyRegistrationId() {
+	return remoteProxyRegistrationId;
+    }
+
+    public void setRemoteProxyRegistrationId(String remoteProxyRegistrationId) {
+	this.remoteProxyRegistrationId = remoteProxyRegistrationId;
+    }
+
+	public String getRemoteMemberId() {
+		return remoteMemberId;
+	}
+
+	public void setRemoteMemberId(String remoteMemberId) {
+		this.remoteMemberId = remoteMemberId;
+	}
+
+}

7ac7ff4272f01df3298fc055f403eb177fbdc1ac
Alvaro Fides <alfiva@itaca.upv.es>
2013-09-05 08:17:50 +0000

    branch for android

---
+++ ri.gateway.communicator/src/main/java/org/universAAL/ri/gateway/eimanager/impl/EIOperationManager.java
@@ -0,0 +1,100 @@
+/*
+Copyright 2011-2014 AGH-UST, http://www.agh.edu.pl
+Faculty of Computer Science, Electronics and Telecommunications
+Department of Computer Science 
+
+See the NOTICE file distributed with this work for additional
+information regarding copyright ownership
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package org.universAAL.ri.gateway.eimanager.impl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.osgi.framework.ServiceReference;
+import org.universAAL.middleware.container.SharedObjectListener;
+import org.universAAL.ri.gateway.communicator.Activator;
+import org.universAAL.ri.gateway.eimanager.ExportOperationInterceptor;
+import org.universAAL.ri.gateway.eimanager.ImportOperationInterceptor;
+import org.universAAL.ri.gateway.eimanager.exception.InterruptExecutionException;
+import org.universAAL.ri.gateway.eimanager.impl.importing.ImportRequest;
+
+public enum EIOperationManager implements SharedObjectListener{
+	Instance;
+	
+	private ImportInterceptorsComparator importComparator;
+	private ExportInterceptorsComparator exportComparator;
+	private List<ImportOperationInterceptor> importList;
+	private List<ExportOperationInterceptor> exportList;
+	
+	{
+		importComparator = new ImportInterceptorsComparator();
+		exportComparator = new ExportInterceptorsComparator();
+		
+		importList = new ArrayList<ImportOperationInterceptor>();
+		exportList = new ArrayList<ExportOperationInterceptor>();
+	}
+	
+	public void init(){
+			Activator.mc.getContainer()
+					.fetchSharedObject(Activator.mc ,
+						new Object[] { ImportOperationInterceptor.class.getName() }, this);
+
+			Activator.mc.getContainer()
+					.fetchSharedObject(Activator.mc ,
+						new Object[] { ExportOperationInterceptor.class.getName() }, this);
+	}
+	
+	public void executeExportOperationChain(ImportRequest request, Type type) throws InterruptExecutionException{
+		List<ExportOperationInterceptor> interceptors = exportList;
+		Collections.sort(interceptors, exportComparator);
+		for(ExportOperationInterceptor i : interceptors){
+			i.process(request);
+		}
+	}
+	
+	public void executeImportOperationChain(ImportRequest request, Type type) throws InterruptExecutionException{
+		List<ImportOperationInterceptor> interceptors = importList;
+		Collections.sort(interceptors, importComparator);
+		for(ImportOperationInterceptor i : interceptors){
+			i.process(request);
+		}
+	}
+	
+	public enum Type {
+		Service,Context,UI;
+	}
+
+	public void sharedObjectAdded(Object sharedObj, Object removeHook) {
+		if (sharedObj instanceof ImportOperationInterceptor){
+			importList.add((ImportOperationInterceptor) sharedObj);
+		}else if (sharedObj instanceof ExportOperationInterceptor){
+			exportList.add((ExportOperationInterceptor) sharedObj);
+		}
+	}
+
+	public void sharedObjectRemoved(Object removeHook) {
+		if (removeHook instanceof ServiceReference){
+			ServiceReference ref = (ServiceReference)removeHook;
+			Object removedService = Activator.bc.getService(ref);
+			if (removedService instanceof ImportOperationInterceptor){
+				importList.add((ImportOperationInterceptor) removedService);
+			}else if (removedService instanceof ExportOperationInterceptor){
+				exportList.add((ExportOperationInterceptor) removedService);
+			}
+		}
+	}
+}
---
+++ ri.gateway.communicator/src/main/java/org/universAAL/ri/gateway/eimanager/impl/exporting/ProxyServiceCaller.java
@@ -0,0 +1,108 @@
+/*
+Copyright 2011-2014 AGH-UST, http://www.agh.edu.pl
+Faculty of Computer Science, Electronics and Telecommunications
+Department of Computer Science 
+
+See the NOTICE file distributed with this work for additional
+information regarding copyright ownership
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package org.universAAL.ri.gateway.eimanager.impl.exporting;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.universAAL.middleware.bus.model.AbstractBus;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.service.DefaultServiceCaller;
+import org.universAAL.middleware.service.ServiceBus;
+import org.universAAL.middleware.service.ServiceBusFacade;
+import org.universAAL.middleware.service.ServiceCall;
+import org.universAAL.middleware.service.ServiceCallee;
+import org.universAAL.middleware.service.ServiceResponse;
+import org.universAAL.middleware.service.owls.profile.ServiceProfile;
+import org.universAAL.ri.gateway.communicator.Activator;
+import org.universAAL.ri.gateway.eimanager.impl.ProxyBusMember;
+
+public class ProxyServiceCaller extends ProxyBusMember {
+
+	private DefaultServiceCaller caller;
+	private String serverNamespace;
+	private String serviceType;
+	private Map<String, List<ServiceProfile>> profiles;
+	private ServiceBus bus;
+
+	public ProxyServiceCaller(final ExportedProxyManager manager,
+			final String targetId, final ModuleContext mc,
+			final String serverNamespace, final String serviceType,
+			final Map<String, List<ServiceProfile>> profiles) {
+		super(manager, targetId, "", mc);
+		caller = new DefaultServiceCaller(mc);
+		this.serverNamespace = serverNamespace;
+		this.serviceType = serviceType;
+		this.profiles = profiles;
+		bus = ServiceBusFacade.fetchBus(Activator.mc);
+	}
+
+	public ServiceResponse invoke(final ServiceCall call, final String memberId) {
+		return ((ServiceCallee) ((AbstractBus) bus).getBusMember(memberId))
+				.handleCall(call);
+	}
+
+	/*
+	 * DEPRECATED
+	 * 
+	 * private void prepareRequestedOutput(final List outputs) { if (outputs !=
+	 * null && !outputs.isEmpty()) { for (int i = outputs.size() - 1; i > -1;
+	 * i--) { ProcessOutput po = (ProcessOutput) outputs.remove(i);
+	 * 
+	 * Object val = po.getParameterValue(); if (val == null) continue;
+	 * 
+	 * String poSuffixUri = po.getURI().substring( po.getURI().indexOf("#") +
+	 * 1);
+	 * 
+	 * ProcessOutput substitutedWithServerURI = new ProcessOutput(
+	 * serverNamespace + poSuffixUri);
+	 * substitutedWithServerURI.setParameterValue(val);
+	 * outputs.add(substitutedWithServerURI); } } }
+	 */
+
+	@Override
+	public void removeProxy() {
+		caller.close();
+	}
+
+	@Override
+	public String getId() {
+		return caller.getMyID();
+	}
+
+	public String getServiceType() {
+		return serviceType;
+	}
+
+	public ServiceProfile[] getProfiles() {
+		List<ServiceProfile> profilesList = new ArrayList<ServiceProfile>();
+		for (List<ServiceProfile> value : profiles.values()) {
+			profilesList.addAll(value);
+		}
+		return profilesList.toArray(new ServiceProfile[0]);
+	}
+
+	public void setProfiles(final Map<String, List<ServiceProfile>> profiles) {
+		this.profiles = profiles;
+	}
+
+}

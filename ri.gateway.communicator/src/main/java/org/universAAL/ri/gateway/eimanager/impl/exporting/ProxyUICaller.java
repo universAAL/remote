44a497cc7410829d579e4e9f1cad912a2d4ece8c
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-12-06 08:44:14 +0000

    adaptations to UI bus api 2.0.6-SNAPSHOT .

--- ri.gateway.communicator/src/main/java/org/universAAL/ri/gateway/eimanager/impl/exporting/ProxyUICaller.java
+++ ri.gateway.communicator/src/main/java/org/universAAL/ri/gateway/eimanager/impl/exporting/ProxyUICaller.java
@@ -23,6 +23,7 @@ package org.universAAL.ri.gateway.eimanager.impl.exporting;
 import java.io.IOException;
 
 import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.ui.UICaller;
 import org.universAAL.middleware.ui.UIHandlerProfile;
 import org.universAAL.middleware.ui.UIRequest;
@@ -61,7 +62,7 @@ public class ProxyUICaller extends ProxyBusMember {
 		public void communicationChannelBroken() {
 		}
 
-		public void dialogAborted(String dialogID) {
+		public void dialogAborted(String dialogID, Resource data) {
 		}
 
 		public void handleUIResponse(UIResponse input) {

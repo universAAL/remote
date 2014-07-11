808e7e57129a5c940d24e91cd6966e01bef9ba58
Alvaro Fides <alfiva@itaca.upv.es>
2014-07-11 12:09:19 +0000

    First commit of ri.api.manager

---
+++ ri.api.manager/src/main/java/org/universaal/ri/api/manager/RemoteAPI.java
@@ -0,0 +1,183 @@
+package org.universaal.ri.api.manager;
+
+/**
+ * Interface that defines the methods of the Remote API, that will be available
+ * through the server HTTP endpoint, according to the R-API documentation.
+ * 
+ * @author alfiva
+ * 
+ */
+public interface RemoteAPI {
+    /**
+     * Value for REGISTER method parameter in the HTTP request
+     */
+    public static final String METHOD_REGISTER = "REGISTER";
+    /**
+     * Value for SENDC method parameter in the HTTP request
+     */
+    public static final String METHOD_SENDC = "SENDC";
+    /**
+     * Value for SUBSCRIBEC method parameter in the HTTP request
+     */
+    public static final String METHOD_SUBSCRIBEC = "SUBSCRIBEC";
+    /**
+     * Value for CALLS method parameter in the HTTP request
+     */
+    public static final String METHOD_CALLS = "CALLS";
+    /**
+     * Value for PROVIDES method parameter in the HTTP request
+     */
+    public static final String METHOD_PROVIDES = "PROVIDES";
+    /**
+     * Value for UNREGISTER method parameter in the HTTP request
+     */
+    public static final String METHOD_UNREGISTER = "UNREGISTER";
+    /**
+     * Parameter in the HTTP request for the authorization identification
+     */
+    public static final String KEY_AUTH = "auth";
+    /**
+     * Parameter in the HTTP request for the method identification
+     */
+    public static final String KEY_METHOD = "method";
+    /**
+     * Parameter in the HTTP request for the main parameter of the method
+     */
+    public static final String KEY_PARAM = "param";
+    /**
+     * Parameter in a HTTP response for a ServiceCall status
+     */
+    public static final String KEY_STATUS = "status";
+    /**
+     * Delimiter in a HTTP response to indicate the serialized ontological
+     * result in turtle starts in the next line
+     */
+    public static final String FLAG_TURTLE = "TURTLE";
+
+    /**
+     * Register a client remote node in the server.
+     * <p>
+     * This method must be called once before any other method. Subsequent calls
+     * will only update the remote endpoint reference, not restart the
+     * registration.
+     * 
+     * @param id
+     *            The token that uniquely identifies the client remote node
+     * @param remote
+     *            Identifies the client endpoint where the push responses and
+     *            callbacks will be sent by the server
+     * @throws Exception
+     *             If any problem arose during the registration and it was not
+     *             successful
+     */
+    void register(String id, String remote) throws Exception;
+
+    /**
+     * Sends a Context Event.
+     * <p>
+     * The server will automatically create a Context Publisher if this is the
+     * first time the method is called. If the passed event contains
+     * ContextProvider information, it is used to instantiate the Publisher.
+     * Otherwise the missing information is automatically generated: The
+     * Publisher is described as either a Gauge or a Controller depending on if
+     * you are providing Services (you called method PROVIDES before). The URI
+     * of the Provider is then set to
+     * <i>http://ontology.universAAL.org/SimpleUAAL
+     * .owl#ContextEventsProvider</i> and it cannot be changed. The pattern that
+     * describes the provided events of this provider is empty, which means it
+     * can publish any type of event. Notice this Provider info is set ONLY THE
+     * FIRST TIME and cannot be changed by later calls.
+     * 
+     * @param id
+     *            The token that uniquely identifies the client remote node
+     * @param cevent
+     *            The serialized ContextEvent
+     * @throws Exception
+     *             If it was not possible to send the event
+     */
+    void sendC(String id, String cevent) throws Exception;
+
+    /**
+     * Subscribes for Context Events.
+     * <p>
+     * The server will automatically create a new Context Subscriber each time
+     * this method is called. All Context Events that match the pattern will be
+     * sent back to the client endpoint. Since every call to this method creates
+     * a new subscriber, you should be careful with how many times you call this
+     * method. Usually, a limited number of subscriptions is needed. Remember
+     * that you can combine different patterns to be handled by a single
+     * subscriber.
+     * 
+     * @param id
+     *            The token that uniquely identifies the client remote node
+     * @param cpattern
+     *            The serialized ContextEventPattern
+     * @throws Exception
+     *             If it was not possible to subscribe to the events
+     */
+    void subscribeC(String id, String cpattern) throws Exception;
+
+    /**
+     * Calls a Service with a Service Request.
+     * <p>
+     * The server will automatically create an internal Default Service Caller
+     * if this is the first time the method is called. The call to the service
+     * is synchronous: this method returns the response of the call straight
+     * from Service Bus.
+     * 
+     * @param id
+     *            The token that uniquely identifies the client remote node
+     * @param srequest
+     *            The serialized ServiceRequest
+     * @return The serialized ServiceResponse
+     * @throws Exception
+     *             If it was not possible to issue the request
+     */
+    String callS(String id, String srequest) throws Exception;
+
+    /**
+     * Registers a Service Profile.
+     * <p>
+     * The server will automatically create a new Service Callee each time this
+     * method is called. All ServiceCalls addressed to this profile will be sent
+     * back to the client endpoint. Since every call to this method creates a
+     * new callee, you should be careful with how many times you call this
+     * method. Usually, a limited number of callees is needed.
+     * 
+     * @param id
+     *            The token that uniquely identifies the client remote node
+     * @param sprofile
+     *            The serialized ServiceProfile
+     * @throws Exception
+     *             If it was not possible to register the profile
+     */
+    void provideS(String id, String sprofile) throws Exception;
+
+    /**
+     * Unregister a client remote node in the server.
+     * <p>
+     * It frees all associated resources allocated to the client by previous
+     * calls. No callbacks will be sent to the client endpoint after this, and
+     * no further method calls will be allowed.
+     * 
+     * @param id
+     *            The token that uniquely identifies the client remote node
+     * @throws Exception
+     *             If it was not possible to unregister the client
+     */
+    void unregister(String id) throws Exception;
+    
+    /**
+     * Unregister all the client remote node in the server.
+     * <p>
+     * This is only for use when the Remote API module is shutting down. It
+     * frees all associated resources allocated to the all client by previous
+     * calls. No callbacks will be sent to the client endpoint after this, and
+     * no further method calls will be allowed. This avoids uAAL wrappers being
+     * kept running in the MW when there is no one to answer to them. Unlike
+     * unregister(), it does not throw Exception: if a node fails to be
+     * unregistered, an error is logged and it continues with the rest.
+     * 
+     */
+    void unregisterAll();
+}
---
+++ ri.api.manager/src/main/java/org/universaal/ri/api/manager/server/persistence/PersistenceDerby.java
@@ -0,0 +1,371 @@
+package org.universaal.ri.api.manager.server.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+import org.universaal.ri.api.manager.Activator;
+import org.universaal.ri.api.manager.RemoteAPI;
+
+/**
+ * An implementation of the Persistence interface using Apache Derby SQL
+ * Database.
+ * 
+ * @author alfiva
+ * 
+ */
+public class PersistenceDerby implements Persistence {
+
+    /**
+     * Name of the root Database
+     */
+    private static final String DBNAME = "RAPIDB";
+    /**
+     * Instance of the Remote API being used in the manager
+     */
+    private RemoteAPI api;
+    /**
+     * Full URL of the Database
+     */
+    private String dbURL;
+
+    /* (non-Javadoc)
+     * @see org.universaal.ri.api.manager.server.persistence.Persistence#init(org.universaal.ri.api.manager.RemoteAPI)
+     */
+    public void init(RemoteAPI remoteAPI) {
+	this.api = remoteAPI;
+	dbURL = "jdbc:derby:/RAPIPersistence;create=true";
+	Connection conn = null;
+	Statement stmt = null;
+	try {
+	    new org.apache.derby.jdbc.EmbeddedDriver();
+	    
+	    conn = DriverManager.getConnection(dbURL);
+	    stmt = conn.createStatement();
+	    stmt.setQueryTimeout(30);
+
+	    String createREGISTERS = "CREATE TABLE " + DBNAME
+		    + ".registers ( id varchar(512) PRIMARY KEY NOT NULL, remote  varchar(512)," +
+		    " tstmp timestamp )";
+	    String createSUBSCRIBERS = "CREATE TABLE " + DBNAME
+		    + ".subscribers ( id varchar(512) PRIMARY KEY NOT NULL, pattern varchar(5120)," +
+		    " tstmp timestamp, CONSTRAINT subid_fk FOREIGN KEY (id) REFERENCES "
+		    + DBNAME + ".registers(id))";
+	    String createCALLEES = "CREATE TABLE " + DBNAME
+		    + ".callees ( id varchar(512) PRIMARY KEY NOT NULL, profile varchar(5120)," +
+		    " tstmp timestamp, CONSTRAINT calid_fk FOREIGN KEY (id)  REFERENCES "
+		    + DBNAME + ".registers(id))";
+
+	    // create tables. Put individual try/catch to ignore already created. I dont trust addBatch.
+	    try {
+		stmt.executeUpdate(createREGISTERS);
+	    } catch (SQLException e) {
+		if (!e.getSQLState().equals("X0Y32")) {
+		    Activator.logI("PersistenceDerby.init", "Database already exists");
+		}
+	    }
+	    try {
+		stmt.executeUpdate(createSUBSCRIBERS);
+	    } catch (SQLException e) {
+		if (!e.getSQLState().equals("X0Y32")) {
+		    Activator.logI("PersistenceDerby.init", "Database already exists");
+		}
+	    }
+	    try {
+		stmt.executeUpdate(createCALLEES);
+	    } catch (SQLException e) {
+		if (!e.getSQLState().equals("X0Y32")) {
+		    Activator.logI("PersistenceDerby.init", "Database already exists");
+		}
+	    }
+
+	} catch (Exception e) {
+	    Activator.logE("PersistenceDerby.init", "Error creating database");
+	    e.printStackTrace();
+	} finally {
+	    try {
+		if (stmt != null) {
+		    stmt.close();
+		}
+		if (conn != null) {
+		    conn.close();
+		}
+	    } catch (SQLException e) {
+		Activator.logE("PersistenceDerby.init", "Error creating database");
+		e.printStackTrace();
+	    }
+	}
+	
+	removeOlderThan(-1l);// -1 : Disabled
+    }
+
+    /* (non-Javadoc)
+     * @see org.universaal.ri.api.manager.server.persistence.Persistence#storeRegister(java.lang.String, java.lang.String)
+     */
+    public void storeRegister(String id, String remote) {
+	Timestamp t = new Timestamp(System.currentTimeMillis());
+	String storeREGISTERS = "insert into " + DBNAME
+		+ ".registers (id, remote, tstmp) values ('" + id + "','"
+		+ remote + "','" + t.toString() + "')";
+	executeGeneric(storeREGISTERS);
+    }
+
+    /* (non-Javadoc)
+     * @see org.universaal.ri.api.manager.server.persistence.Persistence#storeSubscriber(java.lang.String, java.lang.String)
+     */
+    public void storeSubscriber(String id, String pattern) {
+	Timestamp t = new Timestamp(System.currentTimeMillis());
+	String storeREGISTERS = "insert into " + DBNAME
+		+ ".subscribers (id, pattern, tstmp) values ('" + id + "','"
+		+ pattern + "','" + t.toString() + "')";
+	executeGeneric(storeREGISTERS);
+    }
+
+    /* (non-Javadoc)
+     * @see org.universaal.ri.api.manager.server.persistence.Persistence#storeCallee(java.lang.String, java.lang.String)
+     */
+    public void storeCallee(String id, String profile) {
+	Timestamp t = new Timestamp(System.currentTimeMillis());
+	String storeREGISTERS = "insert into " + DBNAME
+		+ ".callees (id, profile, tstmp) values ('" + id + "','"
+		+ profile + "','" + t.toString() + "')";
+	executeGeneric(storeREGISTERS);
+    }
+    
+    /**
+     * Convenient method to execute storeXXX methods of the interface, through a
+     * SQL INSERT (though it could be any SQL sentence that requires using
+     * .execute)
+     * 
+     * @param sql
+     *            The SQL instruction to execute
+     */
+    private void executeGeneric(String sql){
+	Connection conn = null;
+	Statement stmt = null;
+	try {
+	    new org.apache.derby.jdbc.EmbeddedDriver();
+	    
+	    conn = DriverManager.getConnection(dbURL);
+	    stmt = conn.createStatement();
+	    stmt.setQueryTimeout(30);
+	    stmt.executeUpdate(sql);
+
+	} catch (Exception e) {
+	    Activator.logE("PersistenceDerby.executeGeneric", "Error writing in the database");
+	    e.printStackTrace();
+	} finally {
+	    try {
+		if (stmt != null) {
+		    stmt.close();
+		}
+		if (conn != null) {
+		    conn.close();
+		}
+	    } catch (SQLException e) {
+		Activator.logE("PersistenceDerby.executeGeneric", "Error writing in the database");
+		e.printStackTrace();
+	    }
+	}
+    }
+    
+    /* (non-Javadoc)
+     * @see org.universaal.ri.api.manager.server.persistence.Persistence#removeRegister(java.lang.String)
+     */
+    public void removeRegister(String id) {
+	Connection conn = null;
+	Statement stmt = null;
+	try {
+	    new org.apache.derby.jdbc.EmbeddedDriver();
+
+	    conn = DriverManager.getConnection(dbURL);
+	    stmt = conn.createStatement();
+	    stmt.setQueryTimeout(30);
+
+	    String deleteCALLEES = "delete from " + DBNAME + ".callees WHERE id='" + id + "'";
+	    String deleteSUBSCRIBERS = "delete from " + DBNAME + ".subscribers WHERE id='" + id + "'";
+	    String deleteREGISTERS = "delete from " + DBNAME + ".registers WHERE id='" + id + "'";
+	    // create tables. Again, put individual try/catch to keep deleting if errors. I dont trust addBatch.
+	    try {
+		stmt.executeUpdate(deleteCALLEES);
+	    } catch (SQLException e) {
+		if (!e.getSQLState().equals("X0Y32")) {
+		    e.printStackTrace();
+		}
+	    }
+	    try {
+		stmt.executeUpdate(deleteSUBSCRIBERS);
+	    } catch (SQLException e) {
+		if (!e.getSQLState().equals("X0Y32")) {
+		    e.printStackTrace();
+		}
+	    }
+	    try {
+		stmt.executeUpdate(deleteREGISTERS);// TODO Remove in cascade
+	    } catch (SQLException e) {
+		if (!e.getSQLState().equals("X0Y32")) {
+		    e.printStackTrace();
+		}
+	    }
+
+	} catch (Exception e) {
+	    Activator.logE("PersistenceDerby.removeRegister", "Error removing from the database");
+	    e.printStackTrace();
+	} finally {
+	    try {
+		if (stmt != null) {
+		    stmt.close();
+		}
+		if (conn != null) {
+		    conn.close();
+		}
+	    } catch (SQLException e) {
+		Activator.logE("PersistenceDerby.removeRegister", "Error removing from the database");
+		e.printStackTrace();
+	    }
+	}
+    }
+
+    /* (non-Javadoc)
+     * @see org.universaal.ri.api.manager.server.persistence.Persistence#restore()
+     */
+    public void restore() {
+	Connection conn = null;
+	Statement stmt = null;
+	try {
+	    new org.apache.derby.jdbc.EmbeddedDriver();
+	    
+	    conn = DriverManager.getConnection(dbURL);
+	    stmt = conn.createStatement();
+	    stmt.setQueryTimeout(30);
+
+	    String selectREGISTERS="Select * from "+DBNAME+".registers";
+	    String selectSUSBCRIBERS="Select * from "+DBNAME+".subscribers";
+	    String selectCALLEES="Select * from "+DBNAME+".callees";
+	    
+	    ResultSet resultSet = stmt.executeQuery(selectREGISTERS); 
+	    while (resultSet.next()){
+		String id=resultSet.getString(1);
+		String remote=resultSet.getString(2);
+		if(id!=null && remote!=null){
+		    this.api.register(id, remote);
+		}
+	    }
+	    // No need to link from id to id between tables,
+	    // just iterate once everything is registered
+	    resultSet = stmt.executeQuery(selectSUSBCRIBERS);
+	    while (resultSet.next()){
+		String id=resultSet.getString(1);
+		String pattern=resultSet.getString(2);
+		if(id!=null && pattern!=null){
+		    this.api.subscribeC(id, pattern);
+		}
+	    }
+	    
+	    resultSet = stmt.executeQuery(selectCALLEES);
+	    while (resultSet.next()){
+		String id=resultSet.getString(1);
+		String profile=resultSet.getString(2);
+		if(id!=null && profile!=null){
+		    this.api.provideS(id, profile);
+		}
+	    }
+
+	} catch (Exception e) {
+	    Activator.logE("PersistenceDerby.restore", "Error restoring the database");
+	    e.printStackTrace();
+	} finally {
+	    try {//javadoc: resultSet auto closed if stmt is closed
+		if (stmt != null) {
+		    stmt.close();
+		}
+		if (conn != null) {
+		    conn.close();
+		}
+	    } catch (SQLException e) {
+		Activator.logE("PersistenceDerby.restore", "Error restoring the database");
+		e.printStackTrace();
+	    }
+	}
+    }
+
+    /**
+     * Maintenance method that removes all stored data from all tables that was
+     * introduced more than @param millis ago. Removal is not accurate to the
+     * millisecond, it rounds to the day.
+     * 
+     * @param millis
+     *            Minimum age, in milliseconds, at which the data that will be
+     *            removed (data older than this will be removed)
+     */
+    public void removeOlderThan(Long millis) {
+	if (millis<0l)return;//Disable with millis <0
+	Timestamp t=new Timestamp(System.currentTimeMillis()-millis);//Date up to which remove
+	Connection conn = null;
+	Statement stmt = null;
+	try {
+	    new org.apache.derby.jdbc.EmbeddedDriver();
+
+	    conn = DriverManager.getConnection(dbURL);
+	    stmt = conn.createStatement();
+	    stmt.setQueryTimeout(30);
+
+	    // Remove where t - tstmp > 0 (that is, the date -in days- of the
+	    // row is older than the date up to which remove)
+	    String deleteCALLEES = "delete from " + DBNAME
+		    + ".callees WHERE {fn TIMESTAMPDIFF( SQL_TSI_DAY, tstmp, '"
+		    + t.toString() + "')} > 0";
+	    String deleteSUBSCRIBERS = "delete from "
+		    + DBNAME
+		    + ".subscribers WHERE {fn TIMESTAMPDIFF( SQL_TSI_DAY, tstmp, '"
+		    + t.toString() + "')} > 0";
+	    String deleteREGISTERS = "delete from "
+		    + DBNAME
+		    + ".registers WHERE {fn TIMESTAMPDIFF( SQL_TSI_DAY, tstmp, '"
+		    + t.toString() + "')} > 0";
+	    // create tables. Again, put individual try/catch to keep deleting
+	    // if errors. I dont trust addBatch.
+	    try {
+		stmt.executeUpdate(deleteCALLEES);
+	    } catch (SQLException e) {
+		if (!e.getSQLState().equals("X0Y32")) {
+		    e.printStackTrace();
+		}
+	    }
+	    try {
+		stmt.executeUpdate(deleteSUBSCRIBERS);
+	    } catch (SQLException e) {
+		if (!e.getSQLState().equals("X0Y32")) {
+		    e.printStackTrace();
+		}
+	    }
+	    try {
+		stmt.executeUpdate(deleteREGISTERS);// TODO Remove in cascade
+	    } catch (SQLException e) {
+		if (!e.getSQLState().equals("X0Y32")) {
+		    e.printStackTrace();
+		}
+	    }
+
+	} catch (Exception e) {
+	    Activator.logE("PersistenceDerby.removeOlderThan", "Error cleaning the database");
+	    e.printStackTrace();
+	} finally {
+	    try {
+		if (stmt != null) {
+		    stmt.close();
+		}
+		if (conn != null) {
+		    conn.close();
+		}
+	    } catch (SQLException e) {
+		Activator.logE("PersistenceDerby.removeOlderThan", "Error cleaning the database");
+		e.printStackTrace();
+	    }
+	}
+    }
+
+}

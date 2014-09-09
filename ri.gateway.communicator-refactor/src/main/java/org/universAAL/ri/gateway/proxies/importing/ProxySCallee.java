5d43111c2dc6045ac0f140670a5fb04a84fe118f
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-09-09 10:52:43 +0000

    finalized Proxy Servier Callee.

--- ri.gateway.communicator-refactor/src/main/java/org/universAAL/ri/gateway/proxies/importing/ProxySCallee.java
+++ ri.gateway.communicator-refactor/src/main/java/org/universAAL/ri/gateway/proxies/importing/ProxySCallee.java
@@ -15,24 +15,38 @@
  ******************************************************************************/
 package org.universAAL.ri.gateway.proxies.importing;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.context.ContextEvent;
 import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.service.CallStatus;
 import org.universAAL.middleware.service.ServiceCall;
 import org.universAAL.middleware.service.ServiceCallee;
 import org.universAAL.middleware.service.ServiceResponse;
 import org.universAAL.middleware.service.owls.profile.ServiceProfile;
 import org.universAAL.ri.gateway.Session;
+import org.universAAL.ri.gateway.operations.OperationChain;
+import org.universAAL.ri.gateway.protocol.ErrorMessage;
+import org.universAAL.ri.gateway.protocol.Message;
 import org.universAAL.ri.gateway.protocol.WrappedBusMessage;
 import org.universAAL.ri.gateway.proxies.BusMemberReference;
 import org.universAAL.ri.gateway.proxies.ProxyBusMember;
+import org.universAAL.ri.gateway.proxies.ReferencesManager;
+import org.universAAL.ri.gateway.utils.ArraySet;
 
 /**
  * @author amedrano
  * 
  */
 public class ProxySCallee extends ServiceCallee implements ProxyBusMember {
+
+    private final ReferencesManager refsMngr;
+
+    private Resource[] currentRegParam;
 
     /**
      * @param context
@@ -41,52 +55,105 @@ public class ProxySCallee extends ServiceCallee implements ProxyBusMember {
     public ProxySCallee(final ModuleContext context,
 	    final ServiceProfile[] realizedServices) {
 	super(context, realizedServices);
-	// TODO Auto-generated constructor stub
+	refsMngr = new ReferencesManager();
     }
 
     /** {@inheritDoc} */
     @Override
     public void communicationChannelBroken() {
-	// TODO Auto-generated method stub
+	// XXX disconnect?
 
     }
 
     /** {@inheritDoc} */
     @Override
     public ServiceResponse handleCall(final ServiceCall call) {
-	// TODO Auto-generated method stub
-	return null;
+	final Collection<BusMemberReference> refs = refsMngr
+		.getRemoteProxiesReferences();
+	final List<ServiceResponse> responses = new ArrayList<ServiceResponse>();
+	for (final BusMemberReference bmr : refs) {
+	    try {
+		final Session s = bmr.getChannel();
+		if (call.getScopes().contains(s.getScope())) {
+		    // in case the scope is the same as the call ignore.
+		    continue;
+		}
+		if (s.getOutgoingMessageOperationChain().check(call)
+			.equals(OperationChain.OperationResult.ALLOW)) {
+		    // it is allowed to go there
+		    final ContextEvent copy = (ContextEvent) call.deepCopy();
+		    copy.clearScopes();
+		    final Message resp = s.sendRequest(new WrappedBusMessage(
+			    bmr.getBusMemberid(), copy));
+		    // sends a scope clear call to remote proxy.
+		    if (resp != null && resp instanceof WrappedBusMessage) {
+			final ServiceResponse sr = (ServiceResponse) ((WrappedBusMessage) resp)
+				.getMessage();
+			sr.clearScopes();
+			sr.addScope(s.getScope());
+
+			responses.add(sr);
+		    } else if (resp instanceof ErrorMessage) {
+			final ErrorMessage em = (ErrorMessage) resp;
+			LogUtils.logError(
+				owner,
+				getClass(),
+				"handleCall",
+				"Received Error Message: "
+					+ em.getDescription());
+		    } else {
+			LogUtils.logError(owner, getClass(), "handleCall",
+				"unexpected Response.");
+		    }
+		} else {
+		    // it is not allowed, inform caller.
+		    final ServiceResponse sr = new ServiceResponse(
+			    CallStatus.denied);
+		    sr.addScope(s.getScope());
+		    responses.add(sr);
+		}
+	    } catch (final Exception e) {
+		LogUtils.logError(owner, getClass(), "handleCall",
+			new String[] { "Unexpected exception" }, e);
+	    }
+	}
+	// TODO merge all responses
+	if (responses.size() > 0) {
+	    return responses.get(0);
+	}
+	final ServiceResponse sr = new ServiceResponse(CallStatus.denied);
+	sr.setResourceComment("Unable to get any response from remote Proxies.");
+	return sr;
     }
 
     public String getBusMemberId() {
-	// TODO Auto-generated method stub
-	return null;
+	return busResourceURI;
     }
 
+    /** {@inheritDoc} */
     public void addRemoteProxyReference(final BusMemberReference remoteReference) {
-	// TODO Auto-generated method stub
-
+	refsMngr.addRemoteProxyReference(remoteReference);
     }
 
+    /** {@inheritDoc} */
     public void removeRemoteProxyReference(
 	    final BusMemberReference remoteReference) {
-	// TODO Auto-generated method stub
-
+	refsMngr.removeRemoteProxyReference(remoteReference);
     }
 
+    /** {@inheritDoc} */
     public void removeRemoteProxyReferences(final Session session) {
-	// TODO Auto-generated method stub
-
+	refsMngr.removeRemoteProxyReferences(session);
     }
 
+    /** {@inheritDoc} */
     public Collection<BusMemberReference> getRemoteProxiesReferences() {
-	// TODO Auto-generated method stub
-	return null;
+	return refsMngr.getRemoteProxiesReferences();
     }
 
+    /** {@inheritDoc} */
     public Resource[] getSubscriptionParameters() {
-	// TODO Auto-generated method stub
-	return null;
+	return currentRegParam;
     }
 
     public void handleMessage(final Session session,
@@ -95,19 +162,27 @@ public class ProxySCallee extends ServiceCallee implements ProxyBusMember {
 
     }
 
+    /** {@inheritDoc} */
     public boolean isCompatible(final Resource[] registrationParameters) {
-	// TODO Auto-generated method stub
-	return false;
+	return registrationParameters.length > 0
+		&& registrationParameters[0] instanceof ServiceProfile
+		&& new ArraySet.Equal<Resource>().equal(registrationParameters,
+			currentRegParam);
     }
 
+    /** {@inheritDoc} */
     public void addSubscriptionParameters(final Resource[] newParams) {
-	// TODO Auto-generated method stub
+	currentRegParam = new ArraySet.Union<Resource>().combine(
+		currentRegParam, newParams);
+	addNewServiceProfiles((ServiceProfile[]) newParams);
 
     }
 
+    /** {@inheritDoc} */
     public void removeSubscriptionParameters(final Resource[] newParams) {
-	// TODO Auto-generated method stub
-
+	currentRegParam = new ArraySet.Difference<Resource>().combine(
+		currentRegParam, newParams);
+	removeMatchingProfiles((ServiceProfile[]) newParams);
     }
 
 }

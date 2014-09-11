894c7f64eb75d37a334c92106f581383473a5f33
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-09-11 15:13:27 +0000

    Old classes removed.

--- ri.gateway.communicator-refactor/src/main/java/org/universAAL/ri/gateway/communicator/service/impl/GatewayAddress.java
+++ ri.gateway.communicator-refactor/src/main/java/org/universAAL/ri/gateway/communicator/service/impl/GatewayAddress.java
@@ -17,32 +17,42 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+ */
 package org.universAAL.ri.gateway.communicator.service.impl;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class GatewayAddress {
-    
+
+    /**
+     * Alias' prefix under which the HTTP servlet will be registered.
+     */
+    static String ALIAS_PREFIX = "gateway-communicator";
+
     public final String PROTOCOL = "http";
-    private URL url;
-    
-    public GatewayAddress(final String host, final int port) throws MalformedURLException{
-	this.url = new URL(PROTOCOL, host, port, "/"
-		+ CommunicatorStarter
-		.createAlias(null));
+    private final URL url;
+
+    public GatewayAddress(final String host, final int port)
+	    throws MalformedURLException {
+	this.url = new URL(PROTOCOL, host, port, "/" + createAlias(null));
     }
-    
+
     public GatewayAddress(final String host, final int port, final String id)
 	    throws MalformedURLException {
-	this.url = new URL(PROTOCOL, host, port, "/"
-		+ CommunicatorStarter
-		.createAlias(id));
+	this.url = new URL(PROTOCOL, host, port, "/" + createAlias(id));
     }
 
     public URL getUrl() {
 	return url;
+    }
+
+    static String createAlias(final String id) {
+	if (id != null && !"".equals(id)) {
+	    return ALIAS_PREFIX + "-" + id;
+	} else {
+	    return ALIAS_PREFIX;
+	}
     }
 
 }
